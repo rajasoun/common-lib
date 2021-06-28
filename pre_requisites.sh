e62aadc4996e1a9e86d01b683d4fff907a785047
Raja Soundaramourty <rajasoun@icloud.com>
2021-06-28 14:38:04 +0530

    TDD User Input

--- pre_requisites.sh
+++ pre_requisites.sh
@@ -7,12 +7,28 @@ ORANGE=$'\x1B[33m'
 
 # Wrapper function for echo to stderr
 function echo_std_err(){
-    echo "$@" 1>&2
+    echo -e "$@" 1>&2
 }
 
 function check_command_exists () {
-    type "$1" &> /dev/null ;
+    type "$1" &> /dev/null
 } 
+
+function report_success(){
+    SUCCESS=("$@")
+    for cmd in "${SUCCESS[@]}"
+    do
+        echo "${GREEN}✔️ ${cmd} ${NC}"
+    done
+}
+
+function report_failure(){
+   FAILED=("$@")
+   for cmd in "${FAILED[@]}"
+   do
+        echo "${ORANGE}❌ ${cmd} ${NC}"
+   done
+}
 
 function report_results() {
     FAILED=("$@")
@@ -20,18 +36,32 @@ function report_results() {
         echo_std_err "\n💥  ${RED}Failed Checks :" "${#FAILED[@]}${NC}"
         return 1
     else
-        echo  "${GREEN}\n💯  All Passed!${NC}\n"
+        echo -e "\n${GREEN}💯  All Passed!${NC}\n"
         return 0
     fi
 }
 
 function check_pre_requisites() {
    local FAILED=()
+   local SUCCESS=()
    local cmds=("$@")
    for cmd in "${cmds[@]}"
    do 
-        (check_command_exists "$cmd"  && echo "${GREEN}✔️ ${cmd} ${NC}" ) || ( echo "${ORANGE}❌ ${cmd} ${NC}" && FAILED+=("$cmd") )
+        check_command_exists "$cmd" && SUCCESS+=("$cmd") || FAILED+=("$cmd")
    done 
+   report_success "${SUCCESS[@]}"
+   report_failure "${FAILED[@]}"
    report_results "${FAILED[@]}"
 }
 
+function ri_success_test(){
+    #source <(curl -s https://raw.githubusercontent.com/rajasoun/common-lib/main/pre_requisites.sh)
+    cmds=(git make shellspec go fswatch code)
+    check_pre_requisites "${cmds[@]}" 
+}
+
+function ri_failure_test(){
+    #source <(curl -s https://raw.githubusercontent.com/rajasoun/common-lib/main/pre_requisites.sh)
+    cmds=(git make shellspec go fswatch1 code1)
+    check_pre_requisites "${cmds[@]}" 
+}
