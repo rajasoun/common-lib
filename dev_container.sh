ee7015ef8c78a860cc86bdc76020118950746999
Raja Soundaramourty <rajasoun@icloud.com>
2021-06-29 13:41:51 +0530

    build dev-container within ci

--- dev_container.sh
+++ dev_container.sh
@@ -6,6 +6,7 @@ function download_go_dev_container(){
     current_dir="$PWD"
     GO_PATH="$BASE_URL/go"
     wget "$GO_PATH/go-goss.yaml"
+    wget "https://raw.githubusercontent.com/rajasoun/common-lib/main/ci.sh" && chmod +x "ci.sh"
     mkdir -p .devcontainer &&  cd .devcontainer
     wget "$GO_PATH/.devcontainer/devcontainer.json"
     wget "$GO_PATH/.devcontainer/Dockerfile"
