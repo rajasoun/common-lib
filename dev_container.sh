d76298bed35e4427b3358d4cfe163b6723f7f322
Raja Soundaramourty <rajasoun@icloud.com>
2021-06-29 12:43:40 +0530

    download dev container

--- dev_container.sh
+++ dev_container.sh
@@ -3,9 +3,11 @@
 BASE_URL="https://raw.githubusercontent.com/rajasoun/ci-shell-iaac/main/vscode-iaac"
 
 function download_go_dev_container(){
+    current_dir="$PWD"
     GO_PATH="$BASE_URL/go"
     wget "$GO_PATH/go-goss.yaml"
     mkdir -p .devcontainer &&  cd .devcontainer
     wget "$GO_PATH/.devcontainer/devcontainer.json"
     wget "$GO_PATH/.devcontainer/Dockerfile"
+    cd $current_dir
 }
