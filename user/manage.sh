468ffee7aae30b3a9ea5449fdbca61670f8c9c92
Raja Soundaramourty <rajasoun@icloud.com>
2021-07-03 16:10:35 +0530

    User Mgmt

--- user/manage.sh
+++ user/manage.sh
@@ -2,7 +2,7 @@
 
 source <(curl -s https://raw.githubusercontent.com/rajasoun/common-lib/main/ansible/ansible.sh)
 
-if [ $# -le "3" ]; then
+if [ $# -le "2" ]; then
    raise_error "Usage: $0 <vm_name> <vm_ip> <playbook_name.yml>" 
 fi
 
@@ -10,11 +10,15 @@ VM_NAME=$1
 VM_IP=$2
 PLAYBOOK=$3
 
+DEBUG_OFF="" 
+DEBUG_TOGGLE="${4:-$DEBUG_OFF}"
+_debug_option "$DEBUG_TOGGLE"
+
 USER_NAME="ubuntu"
 PURPOSE="_dev_vm"
 
 KEYS_PATH="ssh-keys"
-PRIVATE_KEY="$SSH_KEYS_PATH/id_rsa_$USER_NAME$PURPOSE"
+PRIVATE_KEY="$KEYS_PATH/id_rsa_$USER_NAME$PURPOSE"
 PUBLIC_KEY="${PRIVATE_KEY}.pub"
 
 ANSIBLE_BASE_PATH="ansible"
@@ -22,5 +26,6 @@ CONFIG_BASE_PATH="config"
 
 generate_ssh_keys 
 prepare_ansible 
-ansible_ping 
-configure_vm 
+# ansible_ping 
+# configure_vm 
+rm -fr *.yml *.cfg ssh-keys*.* config roles
