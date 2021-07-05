6827a44c74361036fd09e0f49927d67e150bf817
Raja Soundaramourty <rajasoun@icloud.com>
2021-07-05 13:50:39 +0530

    Update dev_container.sh

--- TddGo/dev_container.sh
+++ TddGo/dev_container.sh
@@ -6,10 +6,11 @@ function download_go_dev_container(){
     current_dir="$PWD"
     mkdir -p vscode-iaac/go
     GO_PATH="$BASE_URL/go"
-    wget "$GO_PATH/go-goss.yaml"
-    wget "https://raw.githubusercontent.com/rajasoun/common-lib/main/ci.sh" && chmod +x "ci.sh"
+    wget -q "$GO_PATH/go-goss.yaml"
+    wget -q "https://raw.githubusercontent.com/rajasoun/common-lib/main/ci.sh" && chmod +x "ci.sh"
+    wget -q "https://raw.githubusercontent.com/rajasoun/common-lib/main/TddGo/pre_checks.sh" && chmod +x "pre_checks.sh"
     mkdir -p .devcontainer &&  cd .devcontainer
-    wget "$GO_PATH/.devcontainer/devcontainer.json"
-    wget "$GO_PATH/.devcontainer/Dockerfile"
+    wget -q "$GO_PATH/.devcontainer/devcontainer.json"
+    wget -q "$GO_PATH/.devcontainer/Dockerfile"
     cd $current_dir
 }
