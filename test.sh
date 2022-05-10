2389fccbb183120f3c0dedfd94bbc5a1d25c9c8e
Raja Soundaramourty <rajasoun@icloud.com>
2022-05-10 09:17:34 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p ${HOME}/test
-cd {HOME}/test
+cd ${HOME}/test
 git clone https://github.com/rajasoun/mac-onboard
-cd {HOME}/test/mac-onboard
+cd ${HOME}/test/mac-onboard
